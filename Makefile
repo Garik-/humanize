220111839a3989d278c8d170d70b43b79e4fdcd8
Gar|k <garik.djan@gmail.com>
2020-06-03 20:59:13 +0300

    feat: add make

---
+++ Makefile
@@ -0,0 +1,47 @@
+include includes.mk
+
+PHONY: help install build package publish test deploy clean promote lint bootstrap registry-login
+
+APPS ?= scan
+
+.DEFAULT_GOAL := help
+
+
+install: ## download dependencies
+	@go mod download > /dev/null >&1
+
+
+build:	install ## build binary
+	@$(foreach APP, $(APPS), $(MAKE) -C $(APPS_DIR)/$(APP) build ;)
+
+
+package: build  ## package docker image
+	@$(foreach APP, $(APPS), $(MAKE) -C $(APPS_DIR)/$(APP) package ;)
+
+
+publish: ## package docker image
+	@$(foreach APP, $(APPS), $(MAKE) -C $(APPS_DIR)/$(APP) publish ;)
+
+promote: ## package docker image
+	@$(foreach APP, $(APPS), $(MAKE) -C $(APPS_DIR)/$(APP) promote ;)
+
+lint:   bootstrap ## run golangci-linter
+	@$(foreach APP, $(APPS), $(MAKE) -C $(APPS_DIR)/$(APP) lint ;)
+
+
+test:   ## run all test suites
+	@$(foreach APP, $(APPS), $(MAKE) -C $(APPS_DIR)/$(APP) test ;)
+
+
+deploy:	## deploy
+	@$(foreach APP, $(APPS), $(MAKE) -C $(APPS_DIR)/$(APP) deploy ;)
+
+
+clean: 	## clean
+	@rm -rf coverage.txt bin/
+	@$(foreach APP, $(APPS), $(MAKE) -C $(APPS_DIR)/$(APP) clean ;)
+
+
+help:
+	@grep -hE '^[ a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | \
+		awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-17s\033[0m %s\n", $$1, $$2}'
---
+++ includes.mk
@@ -0,0 +1,60 @@
+SHELL := /bin/bash
+
+GIT_BRANCH ?= $(shell git rev-parse --abbrev-ref HEAD)
+GIT_HASH ?= $(shell git rev-parse --short HEAD)
+GIT_TAG_HASH ?=
+
+VERSION = $(GIT_BRANCH)-$(GIT_HASH)
+
+DOCKER_REGISTRY ?= test
+DOCKER_REGISTRY_ORG ?=
+DOCKER_CONTEXT ?= ../..
+DOCKERFILE ?= Dockerfile
+DOCKER_TAG_LATEST ?= false
+DOCKER_IMAGE ?= $(DOCKER_REPO)/$(APP):$(VERSION)
+
+GO = go
+GO_FLAGS ?= CGO_ENABLED=0 GOOS=linux GOARCH=amd64
+GO_LDFLAAGS ?= -ldflags="-X 'main.Version=$(VERSION)'"
+GO_HAS_LINT := $(shell command -v golangci-lint;)
+
+BIN ?= bin
+BIN_DIR ?= $(join $(dir $(lastword $(MAKEFILE_LIST))), $(BIN))
+
+APPS_DIR ?= cmd
+
+ifneq (${DOCKER_REGISTRY_ORG},)
+	DOCKER_REPO=$(DOCKER_REGISTRY)/$(DOCKER_REGISTRY_ORG)
+else
+	DOCKER_REPO=$(DOCKER_REGISTRY)
+endif
+
+ifdef TRAVIS_TAG
+	VERSION := $(TRAVIS_TAG)
+	GIT_TAG_HASH := $(shell git rev-list -n 1 $(TRAVIS_TAG) | cut -c1-7)
+endif
+
+ifdef TRAVIS_PULL_REQUEST_BRANCH
+	VERSION = $(TRAVIS_PULL_REQUEST_BRANCH)-$(GIT_HASH)
+endif
+
+ifdef TRAVIS_BRANCH
+	VERSION = $(TRAVIS_BRANCH)-$(GIT_HASH)
+endif
+
+check-bin:
+	@mkdir -p $(BIN_DIR)
+
+bootstrap:
+ifndef GO_HAS_LINT
+	@go get -u github.com/golangci/golangci-lint/cmd/golangci-lint > /dev/null 2>&1
+	# @wget -O- -nv https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sudo sh -s -- -b $(go env GOPATH)/bin v1.27.0
+endif
+
+
+registry-login:
+ifdef DOCKER_PASSWORD
+	@echo $$DOCKER_PASSWORD | docker login -u $$DOCKER_LOGIN $$DOCKER_REGISTRY --password-stdin > /dev/null 2>&1
+else
+	$(error '!!! DOCKER_LOGIN and DOCKER_PASSWORD is required for authentication !!!')
+endif
